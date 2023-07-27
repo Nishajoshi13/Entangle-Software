e9cb7237db85b8c3af44b5473853ca4510224103
Nishajoshi13 <nishajoshi132001@gmail.com>
2023-07-27 10:01:47 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
-import{FormsModule} from '@angular/forms';
+
 import{MatButtonModule} from '@angular/material/button';
 import{MatInputModule} from '@angular/material/input';
 
 import{ReactiveFormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent ,} from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 
@@ -28,11 +29,11 @@ import { ContentComponent } from './content/content.component';
     AppRoutingModule,
     MatToolbarModule,
     MatCardModule,
-    FormsModule,
     MatButtonModule,
     MatInputModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{FormControl,FormGroup,Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent {
-  formData={
-    firstName:'',
-    LastName:'',
-    email:'',
-    Contactno:'',
-    message:'',
-   }
+export class ContentComponent implements OnInit{
+MyForm: FormGroup;
+
+constructor(private http:HttpClient){
+  this.MyForm=new FormGroup({
+  })
+}
+
+  ngOnInit(){
+    this.MyForm=new FormGroup({
+      'FirstName': new FormControl( Validators.required),
+      'LastName': new FormControl(null, [Validators.required ,Validators.email]),
+      'email': new FormControl(null, Validators.required),
+      'Contact No': new FormControl(null, Validators.required),
+      'Message': new FormControl(null),
+    });
   }
+    onSubmit(){
+      console.log(this.MyForm);
+      this.http.get('https://jsonplaceholder.typicode.com/todos/1')
+      .subscribe(posts =>{
+        console.log(posts);
+      });
+    }
+   
+}
 
